68c67a0592983a65632d66a16e651c1f5fe85217
aprakoso98 <adhyt.scott@gmail.com>
2022-03-08 15:26:43 +0700

    0.1.1

--- pages/app/wallet-detail.tsx
+++ pages/app/wallet-detail.tsx
@@ -14,30 +14,35 @@ import {
   Wrapper,
 } from "../../components";
 import { getConfig } from "../../services/binance";
+import { COLORS } from "../../constans";
 
 const WalletDetail = () => {
   const router = useRouter();
   const asset = router.query.from as string;
-  const stableCoin = (router.query.to as string) ?? "USDT";
-  const [assett, setAssett] = useState(asset ?? "");
+  const [assett, setAssett] = useState("");
   const [allOrders, setAllOrders] = useState([] as RetAllOrders);
+  const [display, setDisplay] = useState(false);
+  const [averageShown, setAverageShown] = useState(false);
+  const h = generateChartData(allOrders, asset);
 
-  const { datasets, averageDatasets, labels } = generateChartData(allOrders);
-
-  const setAssetFix = (from: string, to = stableCoin) => {
+  const setAssetFix = (from: string) => {
     router.push({
       pathname: "/app/wallet-detail",
-      query: { from, to },
+      query: { from },
     });
   };
 
   useEffect(() => {
     const getDetail = async () => {
-      const resp = await binance.allOrders(asset, stableCoin);
-      setAllOrders(resp);
+      const resp = await binance.allOrders(asset, "USDT");
+      const resp2 = await binance.allOrders(asset, "BUSD");
+      setAllOrders([...resp, ...resp2]);
     };
-    if (asset?.length > 0) getDetail();
-  }, [stableCoin, asset]);
+    if (asset?.length > 0) {
+      getDetail();
+      setAssett(asset);
+    }
+  }, [asset]);
 
   useLayoutEffect(() => {
     const { hasConfig } = getConfig();
@@ -48,7 +53,11 @@ const WalletDetail = () => {
     <Container>
       <Wrapper>
         <Button onClick={() => router.push("/app")}>Home</Button>
-        <Wrapper flex={0.8}>
+        <Button onClick={() => setDisplay(!display)}>Show Legend</Button>
+        <Button onClick={() => setAverageShown(!averageShown)}>
+          Show Average
+        </Button>
+        <Wrapper flex>
           <Input
             defaultValue={assett}
             onChangeText={setAssett}
@@ -59,46 +68,87 @@ const WalletDetail = () => {
           />
           <Button onClick={() => setAssetFix(assett)}>Search</Button>
         </Wrapper>
-        <Button onClick={() => setAssetFix(assett, "USDT")}>USDT</Button>
-        <Button onClick={() => setAssetFix(assett, "BUSD")}>BUSD</Button>
       </Wrapper>
-      <Wrapper>
-        {averageDatasets.map(({ average, label }) => {
-          return (
-            <View itemsCenter flex key={label}>
-              <Text>{label}</Text>
-              <Text>{`Average : ${average.y}`}</Text>
-              <Text>{`Total : ${average.x}`}</Text>
-            </View>
-          );
-        })}
-      </Wrapper>
-      <View flex>
-        <Chart
-          type="line"
-          options={{
-            spanGaps: true,
-            plugins: {
-              tooltip: {
-                callbacks: {
-                  title: ([{ label }]) => label?.split?.(",").join(" "),
-                  afterBody([{ raw }]) {
-                    const { x } = raw as ScatterDataPoint;
-                    return `Quantity : ${x}`;
-                  },
-                },
-              },
-              title: {
-                display: true,
-                text: `${stableCoin}/${asset}`,
-              },
-            },
-          }}
-          data={{ datasets, labels }}
-        />
-      </View>
+      {averageShown && <RenderAverage {...h} />}
+      <RenderChart {...h} display={display} />
     </Container>
   );
 };
 
 export default WalletDetail;
+
+type HJJ = ReturnType<typeof generateChartData>;
+
+const RenderChart = (props: HJJ & { display: boolean }) => {
+  const { datasets, labels, display } = props;
+  return (
+    <View flex>
+      <Chart
+        type="line"
+        options={{
+          spanGaps: true,
+          plugins: {
+            legend: { position: "right", display },
+            tooltip: {
+              callbacks: {
+                title: ([{ label }]) => label?.split?.(",").join(" "),
+                afterBody([{ raw }]) {
+                  const { x } = raw as ScatterDataPoint;
+                  return `Quantity : ${x}`;
+                },
+              },
+            },
+          },
+        }}
+        data={{ datasets, labels }}
+      />
+    </View>
+  );
+};
+
+const RenderAverage = (props: HJJ) => {
+  const { averageDatasets, datasets } = props;
+  return (
+    <View
+      absolute
+      style={{
+        width: "70%",
+        padding: 16,
+        boxShadow: "5px 5px 20px rgba(0,0,0,0.75)",
+        top: "50%",
+        left: "50%",
+        background: COLORS.white,
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <View
+        style={{
+          display: "grid",
+          rowGap: 16,
+          columnGap: 16,
+          gridTemplateColumns: `repeat(3, 1fr)`,
+        }}
+      >
+        {averageDatasets.map(({ average, label }, index) => {
+          return (
+            <View
+              style={{
+                padding: 10,
+                background: datasets[index].backgroundColor,
+              }}
+              key={label}
+            >
+              <Text style={{ color: COLORS.white }}>{label}</Text>
+              <Text
+                style={{ color: COLORS.white }}
+              >{`Average : ${average.y}`}</Text>
+              <Text
+                style={{ color: COLORS.white }}
+              >{`Total : ${average.x}`}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
--- utils/generateChartData.ts
+++ utils/generateChartData.ts
@@ -4,48 +4,54 @@ import moment from "moment";
 import { COLORS } from "../constans";
 import { RetAllOrders } from "../services/api";
 
-export const generateChartData = (allOrders: RetAllOrders) => {
+export const generateChartData = (allOrders: RetAllOrders, coin: string) => {
   let index = 0;
-  const deftSideAndStatus = { side: [] as string[], status: [] as string[] };
+  const deftSideAndStatus = {
+    symbol: [] as string[],
+    side: [] as string[],
+    status: [] as string[],
+  };
   const deftChartData = {
     labels: [] as string[][],
     dataset: {} as Record<string, ChartDataset<"line">>,
   };
 
   const sideAndStatus = allOrders?.reduce((ret, curr) => {
-    const { status, side } = curr;
+    const { status, side, symbol } = curr;
     if (!ret.status.includes(status)) ret.status.push(status);
     if (!ret.side.includes(side)) ret.side.push(side);
+    if (!ret.symbol.includes(symbol)) ret.symbol.push(symbol);
     return ret;
   }, deftSideAndStatus);
 
   const { dataset, ...rest } = allOrders?.reduce((chartDataset, order) => {
-    const { time, side, status, price, origQty } = order;
+    const { time, side, status, symbol, price, origQty } = order;
     const [pprice, qty] = [parseFloat(price), parseFloat(origQty)];
 
     chartDataset.labels = [
       ...chartDataset.labels,
       moment(time).format(`hh:mm A|DD/MM/YYYY`).split("|"),
     ];
-
-    sideAndStatus.side.forEach((sd) => {
-      sideAndStatus.status.forEach((st) => {
-        const label = `${sd} • ${st}`;
-        const cond = sd === side && st === status;
-        if (!chartDataset.dataset[label]) {
-          chartDataset.dataset[label] = {
-            label,
-            data: [],
-            backgroundColor: Object.values(COLORS)[index],
-            borderColor: Object.values(COLORS)[index],
-          };
-          index++;
-        }
-        chartDataset.dataset[label].data = [
-          // @ts-ignore
-          ...chartDataset.dataset[label]?.data, // @ts-ignore
-          cond ? { x: qty, y: pprice } : { x: null, y: null },
-        ];
+    sideAndStatus.symbol.forEach((sb) => {
+      sideAndStatus.side.forEach((sd) => {
+        sideAndStatus.status.forEach((st) => {
+          const label = `${sb.replace(coin, "")} • ${sd} • ${st}`;
+          const cond = sb === symbol && sd === side && st === status;
+          if (!chartDataset.dataset[label]) {
+            chartDataset.dataset[label] = {
+              label,
+              data: [],
+              backgroundColor: Object.values(COLORS)[index],
+              borderColor: Object.values(COLORS)[index],
+            };
+            index++;
+          }
+          chartDataset.dataset[label].data = [
+            // @ts-ignore
+            ...chartDataset.dataset[label]?.data, // @ts-ignore
+            cond ? { x: qty, y: pprice } : { x: null, y: null },
+          ];
+        });
       });
     });
 
