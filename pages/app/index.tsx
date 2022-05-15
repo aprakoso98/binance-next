ca23598c94729957836c26e941a7bb8c55b9fa3d
bambang.ap <bambang.ap@flash-coffee.com>
2022-05-15 17:07:08 +0700

    finalize p2p history

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,30 +1,37 @@
 import { useRouter } from "next/router";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 import { Button, Container, Text, View, Wrapper } from "../../components";
+import { useArray } from "../../hooks";
 import { binance, RetAccountInfo } from "../../services/api";
 import { getConfig } from "../../services/binance";
 
+type T = Omit<RetAccountInfo, "balances">;
+type Y = T & {
+  balances: ({ hover?: boolean } & RetAccountInfo["balances"][number])[];
+};
+
 const App = () => {
   const router = useRouter();
-  const [data, setData] = useState({} as RetAccountInfo);
+  const [data, setData] = useState({} as Y);
 
   const { balances = [] } = data ?? {};
   const isStableCoins = ["BUSD", "USDT"];
+
   const coins = balances.filter(({ asset }) => !isStableCoins.includes(asset));
   const stableCoins = balances.filter(({ asset }) =>
     isStableCoins.includes(asset)
   );
 
-  const getInfo = async () => {
-    const user = await binance.accountInfo();
-    setData(user);
-  };
-
   const doLogout = () => {
     localStorage.removeItem("apiKey");
     localStorage.removeItem("apiSecret");
     router.replace("/");
+  };
+
+  const getInfo = async () => {
+    const user = await binance.accountInfo();
+    setData(user);
   };
 
   useLayoutEffect(() => {
@@ -35,8 +42,20 @@ const App = () => {
 
   return (
     <Container>
-      <Button onClick={doLogout}>Logout</Button>
-      <Wrapper flex>
+      <Wrapper>
+        <Button style={{ flex: 1, textAlign: "center" }} onClick={doLogout}>
+          Logout
+        </Button>
+        <View style={{ padding: 5 }} />
+        <Button
+          style={{ flex: 1, textAlign: "center" }}
+          onClick={() => router.push("/app/p2p-history")}
+        >
+          P2P History
+        </Button>
+      </Wrapper>
+      <View style={{ padding: 5 }} />
+      <Wrapper style={{ background: "#9fa3b4" }} flex>
         <Text>asset</Text>
         <Text>value</Text>
       </Wrapper>
@@ -44,14 +63,14 @@ const App = () => {
         {stableCoins.map((state) => {
           const { asset, free, locked } = state;
           return (
-            <Wrapper key={asset}>
+            <Wrapper style={{ background: "#9fa3b4" }} key={asset}>
               <Text>{asset}</Text>
               <Text>{free + locked}</Text>
             </Wrapper>
           );
         })}
         {coins.map((state) => {
-          const { asset, free, locked } = state;
+          const { asset, free, locked, hover } = state;
           return (
             <Wrapper
               onClick={() =>
@@ -60,6 +79,7 @@ const App = () => {
                   query: { from: asset },
                 })
               }
+              style={hover ? { background: "#9fa3b4" } : {}}
               key={asset}
             >
               <Text>{asset}</Text>
